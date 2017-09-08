aef032024f983e0ec3038f69d4f49073fb552091
Soni, Kavya <KSoni@mast.com>
2017-09-08 11:17:57 +0530

    Updated packages

--- MyApplication/app/src/main/java/com/tddexample/login/ui/LoginActivity.java
+++ MyApplication/app/src/main/java/com/tddexample/login/ui/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.tddexample;
+package com.tddexample.login.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tddexample.R;
 
 public class LoginActivity extends AppCompatActivity {
 
---
+++ MyApplication/app/src/test/java/com/tddexample/login/presenter/LoginPresenterTest.java
@@ -0,0 +1,10 @@
+package com.tddexample.login.presenter;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ksoni on 08/09/17.
+ */
+public class LoginPresenterTest {
+
+}
